36665c375ef0db13367e24679230998bf6cc10ed
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-04-01 09:41:01 +0200

    debugging

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,11 +5,26 @@ const instance = axios.create({
 })
 
 instance.interceptors.request.use((config) => {
+  // Don't attach token to auth endpoints
+  if (config.url?.startsWith("/auth")) return config
+
   const token = localStorage.getItem("token")
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
+
+// If any request gets a 401, clear token and redirect to login
+instance.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401 && !err.config.url?.startsWith("/auth")) {
+      localStorage.removeItem("token")
+      window.location.href = "/"
+    }
+    return Promise.reject(err)
+  }
+)
 
 export default instance
